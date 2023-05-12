b64ae0d5e800d553cd0fbfb7ec257115be797534
manjot363 <112740405+manjot363@users.noreply.github.com>
2023-05-12 18:51:43 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+prompt("what is your name");
+prompt("what us their name");
+var lovescore=Math.random() *100;
+lovescore=Math.floor(lovescore)+1;
+if(lovescore>70){
+
+    alert("your love score is" +lovescore +"%"+"you love like heer ranjha");
+
+}
+else{
+
+    alert("your love score is"+lovescore + "%");
+}
